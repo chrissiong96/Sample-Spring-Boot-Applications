189b3d077bdf83b10682bb1e79d5392b18fdf75a
chrissiong96 <chrissiong69@gmail.com>
2024-04-27 11:51:23 +0800

    add generate folder

--- src/main/java/com/example/demo/utils/CommonUtils.java
+++ src/main/java/com/example/demo/utils/CommonUtils.java
@@ -7,6 +7,9 @@ import lombok.experimental.UtilityClass;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @UtilityClass
 public class CommonUtils {
@@ -19,6 +22,8 @@ public class CommonUtils {
     }
 
     public static void exportJsonObject(Object response, String fileName, Object... request) throws IOException {
+        Path path = Paths.get("generated_file/");
+        Files.createDirectories(path);
         getObjectMapper().writerWithDefaultPrettyPrinter().writeValue(new File("generated_file/" + fileName + "-request" + ".json"), request);
         getObjectMapper().writerWithDefaultPrettyPrinter().writeValue(new File("generated_file/" + fileName + "-response" + ".json"), response);
     }
